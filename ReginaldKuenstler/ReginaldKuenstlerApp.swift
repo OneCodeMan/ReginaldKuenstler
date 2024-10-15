388c23115b7f5731956afd3bcc6083af804ac096
Dave Gumba <davegumba@gmail.com>
2024-10-15 12:37:39 -0400

    Initial Commit

---
+++ ReginaldKuenstler/ReginaldKuenstlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReginaldKuenstlerApp.swift
+//  ReginaldKuenstler
+//
+//  Created by Dave Gumba on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct ReginaldKuenstlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
