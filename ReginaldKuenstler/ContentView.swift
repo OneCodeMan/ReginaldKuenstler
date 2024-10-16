69c988de68ac0431941e6860ce4a54ab551a4368
Dave Gumba <davegumba@gmail.com>
2024-10-16 18:18:55 -0400

    data model is bad but the functionality is there

--- ReginaldKuenstler/ContentView.swift
+++ ReginaldKuenstler/ContentView.swift
@@ -12,25 +12,65 @@ struct ContentView: View {
     @State var imgTitle = ""
     @State var imgName = ""
     @State var infoString = ""
+    
+    // colours
+    @State var colour1: UIColor = UIColor.red
+    @State var colour2: UIColor = UIColor.red
+    @State var colour3: UIColor = UIColor.red
+    @State var colour4: UIColor = UIColor.red
+    @State var colour5: UIColor = UIColor.red
+    @State var colour6: UIColor = UIColor.red
+    
     var body: some View {
         VStack {
             VStack {
-                Image(uiImage: UIImage(named: "calum")!)
+                Image(uiImage: UIImage(named: "starz")!)
                     .resizable()
-                    .frame(width: 100, height: 300)
+                    .frame(width: 200, height: 200)
+                Text(infoString)
             }
-            .frame(width: 250, height: 60)
-            Text(infoString)
+            HStack {
+                Rectangle()
+                    .fill(Color(colour1))
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(Color(colour2))
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(Color(colour3))
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(Color(colour4))
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(Color(colour5))
+                    .frame(width: 20, height: 20)
+                Rectangle()
+                    .fill(Color(colour6))
+                    .frame(width: 20, height: 20)
+            }
         }
         .padding()
         .onAppear {
             let vm = KuenstlerViewModel()
             self.imgTitle = "Calum from Aftersun"
-            self.imgName = "calum"
+            self.imgName = "starz"
             let image = UIImage(named: imgName)!
             let artwork = Artwork(image: image, title: imgTitle)
-            vm.performAnalOnImage(artwork: artwork)
-            
+            vm.performAnalOnImage(artwork: artwork) { outputColours in
+                DispatchQueue.main.async {
+                    if outputColours.count == 6 {
+                        self.colour1 = outputColours[0]
+                        self.colour2 = outputColours[1]
+                        self.colour3 = outputColours[2]
+                        self.colour4 = outputColours[3]
+                        self.colour5 = outputColours[4]
+                        self.colour6 = outputColours[5]
+                    } else {
+                        print("outputColours has \(outputColours.count) items")
+                    }
+                }
+            }
         }
     }
 }
@@ -50,8 +90,9 @@ struct Palette {
 
 class KuenstlerViewModel {
     
-    func performAnalOnImage(artwork: Artwork) {
+    func performAnalOnImage(artwork: Artwork, completion: @escaping ([UIColor]) -> Void) {
         var infoString = ""
+        var uiColours: [UIColor] = []
         print("performing anal on \(artwork.title)")
         Vibrant.from(artwork.image).getPalette() { palette in
             let p = palette
@@ -85,14 +126,18 @@ class KuenstlerViewModel {
             let inputOutputZip = zip(inputs, outputs)
             
             print("Analysis of \(artwork.title)")
+            
             for (inputColour, outputColour) in inputOutputZip {
-                let yap = "Vibrant took the input as \(inputColour), which corresponds to \(outputColour.name) (hex code: \(outputColour.hexCode))\n"
+                let outputCode = outputColour.rgbCode
+                let currentColour = UIColor(red: CGFloat(outputCode.r) / 255.0, green: CGFloat(outputCode.g) / 255.0, blue: CGFloat(outputCode.b) / 255.0, alpha: 1.0)
+                uiColours.append(currentColour)
+                
+                let yap = "Vibrant took the input as \(inputColour), which corresponds to \(outputColour.name) (hex code: \(outputColour.hexCode))\nThe UIColor for \(outputColour.name) is: \(currentColour)\n\n"
                 infoString += yap
                 print(yap)
             }
+            print("printing uiColours within callback: \(uiColours)")
+            completion(uiColours)
         }
-        
-        // convert the outputs to actual UIColours that u can use on a view
-        
     }
 }
