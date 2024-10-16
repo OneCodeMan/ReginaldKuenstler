6036a07caec7c278d93502005237a33a87169ede
Dave Gumba <davegumba@gmail.com>
2024-10-16 09:43:38 -0400

    first commit

--- ReginaldKuenstler/ContentView.swift
+++ ReginaldKuenstler/ContentView.swift
@@ -6,19 +6,56 @@
 //
 
 import SwiftUI
+import SwiftVibrantium
 
 struct ContentView: View {
+    @State var rectColour = Color(UIColor.red)
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                
+            }
+            .frame(width: 100, height: 50)
+            .background(rectColour)
         }
         .padding()
+        .onAppear {
+            let vm = KuenstlerViewModel()
+            let imgTitle = "Starry Night"
+            let imgName = "starz"
+            let image = UIImage(named: imgName)!
+            let artwork = Artwork(image: image, title: imgTitle)
+            vm.performAnalOnImage(artwork: artwork)
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Artwork {
+    var image: UIImage
+    var title: String
+}
+
+struct Palette {
+    var colours: [String] = []
+}
+
+class KuenstlerViewModel {
+    
+    func performAnalOnImage(artwork: Artwork) {
+        print("performing anal on \(artwork.title)")
+        Vibrant.from(artwork.image).getPalette() { palette in
+            let p = palette
+            let vibrant = p.Vibrant?.hex
+            let darkVibrant = p.DarkVibrant?.hex
+            let lightVibrant = p.LightVibrant?.hex
+            let mutedVibrant = p.Muted?.hex
+            let lightMuted = p.LightMuted?.hex
+            let darkMuted = p.DarkMuted?.hex
+            print("vibrant: \(vibrant);\n darkvibrant: \(darkVibrant);\n lightVibrant: \(lightVibrant); mutedVibrant: \(mutedVibrant); lightMuted: \(lightMuted); darkMuted: \(darkMuted)")
+        }
+    }
 }
