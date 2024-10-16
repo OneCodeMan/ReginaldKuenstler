1deede6a53c65ca5be5d3fa568237a3012f36350
Dave Gumba <davegumba@gmail.com>
2024-10-16 17:37:07 -0400

    got the outputs. need to make them UIColours to be usable

--- ReginaldKuenstler/ContentView.swift
+++ ReginaldKuenstler/ContentView.swift
@@ -9,23 +9,28 @@ import SwiftUI
 import SwiftVibrantium
 
 struct ContentView: View {
-    @State var rectColour = Color(UIColor.red)
+    @State var imgTitle = ""
+    @State var imgName = ""
+    @State var infoString = ""
     var body: some View {
         VStack {
             VStack {
-                
+                Image(uiImage: UIImage(named: "calum")!)
+                    .resizable()
+                    .frame(width: 100, height: 300)
             }
-            .frame(width: 100, height: 50)
-            .background(rectColour)
+            .frame(width: 250, height: 60)
+            Text(infoString)
         }
         .padding()
         .onAppear {
             let vm = KuenstlerViewModel()
-            let imgTitle = "Starry Night"
-            let imgName = "starz"
+            self.imgTitle = "Calum from Aftersun"
+            self.imgName = "calum"
             let image = UIImage(named: imgName)!
             let artwork = Artwork(image: image, title: imgTitle)
             vm.performAnalOnImage(artwork: artwork)
+            
         }
     }
 }
@@ -46,16 +51,48 @@ struct Palette {
 class KuenstlerViewModel {
     
     func performAnalOnImage(artwork: Artwork) {
+        var infoString = ""
         print("performing anal on \(artwork.title)")
         Vibrant.from(artwork.image).getPalette() { palette in
             let p = palette
-            let vibrant = p.Vibrant?.hex
-            let darkVibrant = p.DarkVibrant?.hex
-            let lightVibrant = p.LightVibrant?.hex
-            let mutedVibrant = p.Muted?.hex
-            let lightMuted = p.LightMuted?.hex
-            let darkMuted = p.DarkMuted?.hex
+            let vibrant: String = p.Vibrant?.hex ?? ""
+            let darkVibrant: String = p.DarkVibrant?.hex ?? ""
+            let lightVibrant: String = p.LightVibrant?.hex ?? ""
+            let mutedVibrant: String = p.Muted?.hex ?? ""
+            let lightMuted: String = p.LightMuted?.hex ?? ""
+            let darkMuted: String = p.DarkMuted?.hex ?? ""
             print("vibrant: \(vibrant);\n darkvibrant: \(darkVibrant);\n lightVibrant: \(lightVibrant); mutedVibrant: \(mutedVibrant); lightMuted: \(lightMuted); darkMuted: \(darkMuted)")
+            
+            // take input
+            let inputs = [vibrant, darkVibrant, lightVibrant, mutedVibrant, lightMuted, darkMuted]
+            var outputs: [VColour] = []
+            
+            let mapper = ColourMapper()
+            let colourMap = mapper.createColourMapFromCSV()
+            print("\n\n\n\n")
+            
+            
+            // for every colour input
+            for inputColour in inputs {
+                // convert to rgb
+                let rgbInputTuple = ColourConverter.hexToRGB(hex: inputColour)
+                
+                // calculate nearest distance, output
+                let nearestColour = ColourConverter.findNearestColorFromRGBValue(rgbTuple: rgbInputTuple, colourMap: colourMap)
+                outputs.append(nearestColour)
+            }
+            
+            let inputOutputZip = zip(inputs, outputs)
+            
+            print("Analysis of \(artwork.title)")
+            for (inputColour, outputColour) in inputOutputZip {
+                let yap = "Vibrant took the input as \(inputColour), which corresponds to \(outputColour.name) (hex code: \(outputColour.hexCode))\n"
+                infoString += yap
+                print(yap)
+            }
         }
+        
+        // convert the outputs to actual UIColours that u can use on a view
+        
     }
 }
--- ReginaldKuenstler/Data/ColourMapper.swift
+++ ReginaldKuenstler/Data/ColourMapper.swift
@@ -6,8 +6,47 @@
 //
 
 import Foundation
+import UIKit
 
 // Has a dict of colour string names and their rgb/hex values
 final class ColourMapper {
     
+    // extract `colourmap.csv` and make a dict out of it
+    // dict = { "Cadmium Red": "#FF2210" }
+    var colourMap: [VColour] = []
+    func createColourMapFromCSV() -> [VColour] {
+        var coloursFromCSV: [VColour] = []
+        
+        guard let filePath = Bundle.main.path(forResource: "colourmap", ofType: "csv") else {
+            print("CSV FILE NON-EXISTENT.")
+            return coloursFromCSV
+        }
+        
+        // read from csv
+        do {
+            let csvData = try String(contentsOfFile: filePath, encoding: .utf8)
+            let rows = csvData.components(separatedBy: "\n")
+            
+            for row in rows {
+                let columns = row.components(separatedBy: ",")
+                
+                if columns.count == 2 {
+                    let rgbCode = ColourConverter.hexToRGB(hex: columns[1])
+                    let colour = VColour(name: columns[0], hexCode: columns[1], rgbCode: rgbCode)
+                    coloursFromCSV.append(colour)
+                }
+            }
+        } catch {
+            print("Error reading CSV file: \(error)")
+        }
+        print(coloursFromCSV)
+        self.colourMap = coloursFromCSV
+        return coloursFromCSV
+    }
+}
+
+struct VColour {
+    var name: String = ""
+    var hexCode: String = ""
+    var rgbCode: RGBTuple = (r: 0, g: 0, b: 0)
 }
