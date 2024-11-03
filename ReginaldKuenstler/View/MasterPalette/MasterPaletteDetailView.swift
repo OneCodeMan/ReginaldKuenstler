aa1804f319e5a5b6d4f3d6d542d7c51d46e1dd05
Dave Gumba <davegumba@gmail.com>
2024-11-02 22:53:12 -0400

    a lot of assets added

--- ReginaldKuenstler/View/MasterPalette/MasterPaletteDetailView.swift
+++ ReginaldKuenstler/View/MasterPalette/MasterPaletteDetailView.swift
@@ -5,27 +5,43 @@
 //  Created by Dave Gumba on 2024-11-02.
 //
 
+
+/*
+ LOL
+ https://gist.github.com/cjnevin/fc416e31cfcfb7f5f60a1fb401926f79
+ https://stackoverflow.com/questions/68409874/can-i-create-a-scrollview-that-is-infinite-scrolling-but-has-fixed-values
+ */
+
 import SwiftUI
 
+/*
+ Horizontal Images carousel
+ */
 struct MasterPaletteDetailView: View {
-    let images = ["caravaggio0", "caravaggio1", "caravaggio2", "caravaggio3", "caravaggio4", "caravaggio5"]
+    // let images = ["caravaggio0", "caravaggio1", "caravaggio2", "caravaggio3", "caravaggio4", "caravaggio5"]
+    
+    @State var images: [String] = MasterPaletteConstants.hopperImageStrings
+    
+    let rows = [GridItem(.flexible(minimum: UIScreen.main.bounds.width))]
     
     var body: some View {
-        TabView {
-            ForEach(images, id: \.self) { image in
-                Image(image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.9) // Fill most of the screen
-                    .clipped()
-                    .padding(.bottom, 20)
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows, spacing: 0) {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(minWidth: UIScreen.main.bounds.width ,maxWidth: .infinity, minHeight: UIScreen.main.bounds.height) // Fill most of the screen
+                        .clipped()
+                }
             }
         }
-        .tabViewStyle(PageTabViewStyle())
         .edgesIgnoringSafeArea(.all) // Makes images fill edge to edge
+        .toolbar(.hidden, for: .tabBar)
+        .statusBarHidden()
+        .onAppear {
+            
+        }
     }
-}
-
-#Preview {
-    MasterPaletteDetailView()
+    
 }
