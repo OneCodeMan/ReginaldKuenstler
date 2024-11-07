ea79852bdf042ed1ce2436cc97a2acd3ca275d27
Dave Gumba <davegumba@gmail.com>
2024-11-06 20:43:12 -0500

    progress

--- ReginaldKuenstler/Data/ColourMapper.swift
+++ ReginaldKuenstler/Data/ColourMapper.swift
@@ -15,6 +15,7 @@ struct CatalogColour: Codable, Identifiable {
     let rgb: [Int]
 }
 
+
 // MARK: V0.2
 final class ColourCatalog: ObservableObject {
     
@@ -33,36 +34,35 @@ final class ColourCatalog: ObservableObject {
     }
     
     func createColourMapFromCatalogJSON(completion: @escaping ([CatalogColour]) -> Void) {
-            // Check if colourMap is already populated
-            if !colourMap.isEmpty {
-                completion(colourMap) // Return existing colourMap if already populated
-                return
-            }
-            
-            // Load the JSON data from a file named `catalog.json` in the bundle
-            guard let url = Bundle.main.url(forResource: "catalog", withExtension: "json") else {
-                print("Could not find catalog.json file.")
-                completion([])
-                return
-            }
-            
-            do {
-                // Read the data from the file
-                let data = try Data(contentsOf: url)
-                
-                // Decode the JSON data into [CatalogColour]
-                let catalog = try JSONDecoder().decode([CatalogColour].self, from: data)
-                
-                // Update the colourMap and call the completion handler
-                DispatchQueue.main.async {
-                    self.colourMap = catalog
-                    completion(catalog)
-                }
-            } catch {
-                print("Error decoding JSON: \(error)")
-                completion([]) // Return an empty array in case of error
-            }
+        if !colourMap.isEmpty {
+            completion(colourMap) // Return existing colourMap if already populated
+            return
         }
+        
+// Load the JSON data from a file named `catalog.json` in the bundle
+       guard let url = Bundle.main.url(forResource: "catalog", withExtension: "json") else {
+           print("Could not find catalog.json file.")
+           completion([])
+           return
+       }
+       
+       do {
+           // Read the data from the file
+           let data = try Data(contentsOf: url)
+        
+        // Decode the JSON data into [CatalogColour]
+        let catalog = try JSONDecoder().decode([CatalogColour].self, from: data)
+        
+        // Update the colourMap and call the completion handler
+        DispatchQueue.main.async {
+            self.colourMap = catalog
+            completion(catalog)
+        }
+       } catch {
+                  print("Error decoding JSON: \(error)")
+                  completion([]) // Return an empty array in case of error
+              }
+    }
 }
 
 
